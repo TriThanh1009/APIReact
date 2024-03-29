431167f3b4d55c7636b6761b47aa707861e0effe
ThanhRua <92020142+TriThanh1009@users.noreply.github.com>
2024-03-29 21:46:10 +0700

    commit

---
+++ APIReact/DbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIReact
+{
+    public class DbContextFactory
+    {
+        public DbContextFactory(string[] agrs)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("EmployeeManger");
+            var optionsBuilder = new DbContextOptionsBuilder<EmployeeDbContext>();
+            optionsBuilder.UseSqlServer
+        }
+    }
+}
---
+++ APIReact/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APIReact.Entities;
+
+namespace APIReact
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<EmployeeEntity> Employee { get; set; }
+    }
+}
